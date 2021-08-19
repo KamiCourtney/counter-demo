214f9460061cef869439e053e05a6979c4f8acee
Courtney <CourtAlexBrown@gmail.com>
2021-08-19 11:38:12 -0400

    starting demo

---
+++ main.js
@@ -0,0 +1,7 @@
+let incrementButton = document.querySelector(".increment")
+let decrementButton = document.querySelector(".decrement")
+let countView = document.querySelector(".count")
+
+console.log(incrementButton)
+console.log(decrementButton)
+console.log(countView)
